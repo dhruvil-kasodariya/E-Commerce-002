65c02dae36e2937961031401decc5f5f69682dfd
Dhruvil Kasodariya <97498214+dhruvil-kasodariya@users.noreply.github.com>
2023-10-03 11:20:25 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,5 @@
 export const fetchData = async (url) =>
   await fetch(url).then((res) => res.json());
+
+export const fetchDataWithOption = async (url,option) =>
+  await fetch(url,option).then((res) => res.json());
